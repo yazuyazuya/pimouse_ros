99d88290a33ad1d17a4a650bbc69bfdbaaf3cfbb
yazuyazuya <kzyono-1999@docomo.ne.jp>
2020-09-01 03:27:35 +0000

    fix .travis.yml

---
+++ scripts/lightsensor3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys, rospy
+from pimouse_ros.msg import LightSensorValues
+
+if __name__ == '__main__':
+    freq = rospy.get_param('lightsensors_freq',10)
+        
+    devfile = '/dev/rtlightsensor0'
+    rospy.init_node('rtlightsensors')
+    pub = rospy.Publisher('lightsensors', LightSensorValues, queue_size=1)
+    rate = rospy.Rate(freq)
+
+    while not rospy.is_shutdown():
+        try:
+            with open(devfile,'r') as f:
+                data = f.readline().split()
+                d = LightSensorValues()
+                d.right_forward = int(data[0])
+                d.right_side = int(data[1])
+                d.left_side = int(data[2])
+                d.left_forward = int(data[3])
+                pub.publish(d)
+        except:
+            rospy.logerr("can't open " + devfile)
+
+        rate.sleep()
