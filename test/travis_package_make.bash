4ff0cae6da7062ec3fa28c5742e04788d2b208f2
yazuyazuya <kzyono-1999@docomo.ne.jp>
2020-09-02 12:10:55 +0000

    fix travis_package_make.bash

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xve
 
 #sync and make
-rsync -av --exclude='.git/' ./~/catkin_ws/src/pimouse_ros_2/
+rsync -av --exclude='.git/' ./~/catkin_ws/src/pimouse_ros/
 cd ~/catkin_ws
 catkin_make
