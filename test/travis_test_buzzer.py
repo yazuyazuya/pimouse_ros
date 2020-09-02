5ac1a509da5e0a3f579f376c0cc8ce0e5a8799b5
yazuyazuya <kzyono-1999@docomo.ne.jp>
2020-09-02 13:51:12 +0000

    solve the error of rosnode

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -6,9 +6,9 @@ import time
 class BuzzerTest(unittest.TestCase):
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
-        self.assertIn('/buzzer', nodes, "node does not exist")
+        self.assertIn('/buzzer',nodes, "node does not exist")
 
-if __name__=='__main__':
+if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_buzzer')
-    rostest.rosrun('pimouse_ros', 'travis_test_buzzer', BuzzerTest)
+    rostest.rosrun('pimouse_ros','travis_test_buzzer',BuzzerTest)
